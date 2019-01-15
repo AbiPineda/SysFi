c3d6f2ef6b4aed89af1a36214c86a9bfee6e61f0
JcMoz <31970838+JcMoz@users.noreply.github.com>
2019-01-15 00:12:17 -0600

    factura

--- sysFinanzas/Reportes/factura.php
+++ sysFinanzas/Reportes/factura.php
@@ -30,18 +30,25 @@ while ($m= mysqli_fetch_array($clientecito)){
     $pdf->SetFont('Arial','',11);
     $pdf->Cell(70,6, utf8_decode($m['dir_cliente']),1,1,'L');
 }
+
+$facturita= mysqli_query($conexion,"SELECT c.concepto1, c.valor, c.fecha,d.valor as pUni,d.cantidad, a.nombre,d.importe
+    FROM contable c INNER JOIN detalle d ON d.factura=c.concepto2
+INNER JOIN articulos a ON a.idarticulos=d.articulo WHERE d.factura='$factura'");
+
+while ($w= mysqli_fetch_array($facturita)){
+   $total=$w['valor'];
     $pdf->SetXY(110,30);
     $pdf->SetFillColor(249,249,249);
     $pdf->SetFont('Arial','B',11);
     $pdf->Cell(40,6,utf8_decode('CONDIC. DE PAGO:'),0,0,'L',1);
     $pdf->SetFont('Arial','',11);
-    $pdf->Cell(40,6, utf8_decode(''),1,1,'L');
+    $pdf->Cell(40,6, utf8_decode($w['concepto1']),1,1,'L');
     $pdf->SetXY(110,36);
     $pdf->SetFillColor(249,249,249);
     $pdf->SetFont('Arial','B',11);
     $pdf->Cell(40,6,utf8_decode('FECHA:'),0,0,'L',1);
     $pdf->SetFont('Arial','',11);
-    $pdf->Cell(40,6, utf8_decode(''),1,1,'L');
+    $pdf->Cell(40,6, utf8_decode($w['fecha']),1,1,'L');
 
     $pdf->Ln(10);
     $pdf->SetFillColor(0,186,211);
@@ -54,11 +61,13 @@ while ($m= mysqli_fetch_array($clientecito)){
     $pdf->SetFont('Arial','',9);
    // $pdf->MultiCell(20,6, utf8_decode('Tolerancia Post-Ingesta 75grs. de Glucosa 2hrs.'),1,'L',0,0,0);
    // $pdf->MultiCell(65,6, utf8_decode('Tolerancia Post-Ingesta 75grs. de Glucosa 2hrs.'),1,'L',0,0);
-    $pdf->Cell(20,6, utf8_decode(''),1,0,'L');
-    $pdf->Cell(65,6, utf8_decode(''),1,0,'L');
-    $pdf->Cell(30,6, utf8_decode(''),1,0,'L');
-    $pdf->Cell(35,6, utf8_decode(''),1,0,'L');
-
+    $pdf->Cell(20,6, utf8_decode($w['cantidad']),1,0,'C');
+    $pdf->Cell(65,6, utf8_decode($w['nombre']),1,0,'C');
+    $pdf->Cell(30,6, utf8_decode($w['pUni']),1,0,'C');
+    $pdf->Cell(35,6, utf8_decode($w['importe']),1,1,'C');
+}
+$pdf->Cell(35,6,utf8_decode('Total'),1,1,'C',1);
+$pdf->Cell(35,6, utf8_decode($total),1,0,'L');
 	$pdf->SetFont('Arial','',10);
 	
 	
